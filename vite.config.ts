66bf284f0a8d07173ce73e0c817fae1d584e2567
Little Flower School <admin@littleflowerparsa.edu.np>
2026-08-29 10:00:42 +0545

    Add gh-pages deploy script and relative base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Little-Flower-/',
+  base: './',
   plugins: [
     react(),
     tailwindcss()
